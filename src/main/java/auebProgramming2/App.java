8d3fcab9e230be7d331f0bc8f83dfeaa0498b8c4
Thanos Ilias <t8240051@aueb.gr>
2025-11-08 21:06:25 +0200

    changed url

--- src/main/java/auebProgramming2/App.java
+++ src/main/java/auebProgramming2/App.java
@@ -7,8 +7,10 @@ public final class App {
         Links link = new Links();
         link.linkGovBudget();
 
-        File file = new File("https://minfin.gov.gr/wp-content/uploads/2024/11/%CE%9A%CF%81%CE%B1%CF%84%CE%B9%CE%BA%CF%8C%CF%82-%CE%A0%CF%81%CE%BF%CF%8B%CF%80%CE%BF%CE%BB%CE%BF%CE%B3%CE%B9%CF%83%CE%BC%CF%8C%CF%82-2025_%CE%9F%CE%95.pdf");
+        File file = new File("https://minfin.gov.gr/wp-content/uploads/2024/11/kratikos-proypologismos-2025_ΟΕ.pdf");
         link.extractLines(file);
 
+        ConnectToSql table = new ConnectToSql();
+        table.getTableElements();
     }
 }
