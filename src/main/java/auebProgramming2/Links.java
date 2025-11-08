30e768b357d8bf4b95f5c33eb23d350fc47fea5f
ThanosIlias <t8240051@aueb.gr>
2025-11-08 13:48:37 +0200

    Update Links.java

--- src/main/java/auebProgramming2/Links.java
+++ src/main/java/auebProgramming2/Links.java
@@ -4,7 +4,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Links {
-    public String linkGovBuget() throws Exception {
+    public String linkGovBudget() throws Exception {
         String url = "https://minfin.gov.gr/kratikos-proypologismos/";
         Document doc = Jsoup.connect(url).get();
 
