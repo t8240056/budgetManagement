e3ccf807c0aebf7d04b42b6f843b5aee00c1b8f1
Thanos Ilias <t8240051@aueb.gr>
2025-11-08 21:41:13 +0200

    changed url and problem fixed

--- src/main/java/auebProgramming2/ConnectToSql.java
+++ src/main/java/auebProgramming2/ConnectToSql.java
@@ -5,8 +5,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class ConnectToSql {
-    public void getTableElements() {
+public class ConnectToSql{
+    public void getTableElements() throws Exception {
+        String url = "https://minfin.gov.gr/wp-content/uploads/2024/11/%CE%9A%CF%81%CE%B1%CF%84%CE%B9%CE%BA%CF%8C%CF%82-%CE%A0%CF%81%CE%BF%CF%8B%CF%80%CE%BF%CE%BB%CE%BF%CE%B3%CE%B9%CF%83%CE%BC%CF%8C%CF%82-2025_%CE%9F%CE%95.pdf";
+        Document doc = Jsoup.connect(url).get();
         Elements tables = doc.select("table");
         for (Element table : tables) {
             Elements rows = table.select("tr");
