28ded9d8e6c386150eac080ca3195b81131ba74e
Nikolas Kalantzis <t8240056@aueb.gr>
2025-11-09 16:19:20 +0200

    Changed App's constructor
     made the constructor private

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -1,8 +1,11 @@
 package auebprogramming;
-
 public class App {
     public static void main(String[] args) {
         var link = new Links();
         link.linkGovBudget();
     }
+private App() {
+    
+}
+
 }
