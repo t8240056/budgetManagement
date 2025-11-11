2e63267c770217faa9be2376c6c7787cdea91a75
Thanos Ilias <t8240051@aueb.gr>
2025-11-11 15:27:54 +0200

    added GetPdf file and solved sm problems

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -2,7 +2,7 @@ package auebprogramming;
 
 import java.net.URI;
 import java.net.URL;
-import java.util.ArrayList;
+
 
 /**entry point of the application */
 public class App {
@@ -13,15 +13,13 @@ public class App {
         String pdfUrl = "https://minfin.gov.gr/wp-content/uploads/2024/11/%CE%9A%CF%81%CE%B1%CF%84%CE%B9%CE%BA%CF%8C%CF%82-%CE%A0%CF%81%CE%BF%CF%8B%CF%80%CE%BF%CE%BB%CE%BF%CE%B3%CE%B9%CF%83%CE%BC%CF%8C%CF%82-2025_%CE%9F%CE%95.pdf";
         URI uri = URI.create(pdfUrl);
         URL url = uri.toURL();
-       
+        
+        BudgetPdfProcessor downloader = new BudgetPdfProcessor();
         BudgetPdfProcessor.downloadBudgetPdf();
         // ArrayList<String> lines = link.extractLines(url);       
         // ConnectToSql table = new ConnectToSql();
         //table.getTableElements(pdfUrl);
 
     }
-private App() {
-
-}
 
 }
