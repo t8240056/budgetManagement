0ef5adfd6836d31c9b377d51d47d8063abc30d61
Thanos Ilias <t8240051@aueb.gr>
2025-11-18 18:13:20 +0200

    updated MAIN

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -7,20 +7,20 @@ public class App {
     public static void main(final String[] args) throws Exception {
         //ReadPDF read = new ReadPDF();
         //read.getInFile();
+
+        //making objects to work with 
         Scanner input = new Scanner(System.in);
         Links link = new Links();
         GetPdf download = new GetPdf();
+        //
 
+        //getting the year from the user
         System.out.println("Which year's government budget do you want ?");
         int year = input.nextInt(); 
         
-
-
+        //getting the general link and downloading the pdf
         String urlPdf =link.linkGovBudget(year);
-        //download.fileDownloader(urlPdf);
-        System.out.println(link.getUrlPdf(year));
-
-
+        download.fileDownloader(urlPdf);
         input.close();
     }
 
