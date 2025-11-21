61670f452eedf92deb274ec05ca671a3d9f7eacd
Thanasis Bliachos <t8240086@aueb.gr>
2025-11-21 12:50:22 +0200

    Import Scanner and creation of instance scanner

--- src/main/java/auebprogramming/Main.java
+++ src/main/java/auebprogramming/Main.java
@@ -1,6 +1,9 @@
 package auebprogramming;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
     }
 }
