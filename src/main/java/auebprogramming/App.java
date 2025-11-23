192b9ec3d6e91023fd36978bf4efefffbc415774
Thanos Ilias <t8240051@aueb.gr>
2025-11-23 12:10:12 +0200

    used for loop to download every years output file

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -1,28 +1,23 @@
 package auebprogramming;
 
-import java.util.Scanner;
 
 /**entry point of the application */
 public class App {
-    public static void main(final String[] args) throws Exception {
+    public static void main(final String[] args) {
 
-        //making objects to work with 
-        Scanner input = new Scanner(System.in);
+        //making objects to work with
         Links link = new Links();
         GetPdf download = new GetPdf();
         //
         
-        //getting the year from the user
-        System.out.println("Which year's government budget do you want ? Choose from 2020 until 2025");
-        int year = input.nextInt(); 
-        
-        //getting the general link and downloading the pdf
-        String urlBudget =link.linksForBudget(year); 
-        download.fileDownloader(urlBudget , year);
+        for (int year = 2020 ; year <= 2026 ; year++ ) {
 
-        ReadPDF read = new ReadPDF();
-        read.getInFile(year);
+            //getting the general link and downloading the pdf
+            String urlBudget =link.linksForBudget(year); 
+            download.fileDownloader(urlBudget , year);
 
-        input.close();
+            ReadPDF read = new ReadPDF();
+            read.getInFile(year);
+        }
     }
 }
