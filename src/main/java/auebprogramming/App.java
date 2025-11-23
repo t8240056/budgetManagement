fa38411e5c29c33fb2acc98e54516457d1b52320
Thanos Ilias <t8240051@aueb.gr>
2025-11-23 12:31:38 +0200

    avoided some output warnings and downloaded eatch years pdf's

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -1,6 +1,7 @@
 package auebprogramming;
 
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**entry point of the application */
 public class App {
     public static void main(final String[] args) {
@@ -9,7 +10,11 @@ public class App {
         Links link = new Links();
         GetPdf download = new GetPdf();
         //
-        
+
+        // in order to avoid seeing warning for trying to load ariel font and have a clearer output
+        Logger.getLogger("org.apache.pdfbox").setLevel(Level.SEVERE);
+        //
+
         for (int year = 2020 ; year <= 2026 ; year++ ) {
 
             //getting the general link and downloading the pdf
