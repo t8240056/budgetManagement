7b8519d6c1c1381310de40aac7a1336bc09b46c5
Nikolas Kalantzis <t8240056@aueb.gr>
2025-11-23 14:08:23 +0200

    Fixed package declaration , added one import

--- src/main/java/auebprogramming/CVSmanagement.java
+++ src/main/java/auebprogramming/CVSmanagement.java
@@ -1,8 +1,9 @@
+package auebprogramming;
 import java.io.BufferedReader;
 import java.util.List;
 import java.util.ArrayList;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class CVSmanagement {
