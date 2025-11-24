2faa39c69f17735fa2ee71c6ec8e8450d445b174
Thanos Ilias <t8240051@aueb.gr>
2025-11-25 01:13:05 +0200

    run Article_1 in App

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -1,10 +1,19 @@
 package auebprogramming;
 
+import java.util.Scanner;
+
 /**entry point of the application */
 public class App {
 
     public static void main(final String[] args) {
-        DownloadingOutputFiles.run();
+        // DownloadingOutputFiles.run();  run only once 
+        
+        Scanner scanner = new Scanner(System.in);
+        var article1 = new Article_1();
+        
+        System.out.println(" Which year do you want to see ?");
+        int year = scanner.nextInt();
+        article1.getArticle1(year); 
 
     }
 }
