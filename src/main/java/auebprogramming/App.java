31a0fbef11512a27fbb3d53c53d7a82038858577
Thanos Ilias <t8240051@aueb.gr>
2025-11-24 11:00:37 +0200

    main is running one method

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -1,30 +1,14 @@
 package auebprogramming;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**entry point of the application */
 public class App {
+
+    //we dont want everytime we run the main to download the files from the start
+    static int getFilesOnce = 0;
     public static void main(final String[] args) {
-
-        //making objects to work with
-        Links link = new Links();
-        GetPdf download = new GetPdf();
-        //
-
-        // in order to avoid seeing warning for trying to load ariel font and have a clearer output
-        Logger.getLogger("org.apache.pdfbox").setLevel(Level.SEVERE);
-        //
-        int startYear = 2020;
-        int endYear = 2026;
-
-        for (int year = startYear ; year <= endYear ; year++ ) {
-
-            //getting the general link and downloading the pdf
-            String urlBudget =link.linksForBudget(year); 
-            download.fileDownloader(urlBudget , year);
-
-            ReadPDF read = new ReadPDF();
-            read.getInFile(year);
+        if (getFilesOnce == 0 ) {
+            DownloadingOutputFiles.run();
+            getFilesOnce +=1;            
         }
     }
 }
