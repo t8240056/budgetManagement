f79887ae9deec186c8ab39b9e677409c82d61ab0
Thanos Ilias <t8240051@aueb.gr>
2025-11-24 16:42:16 +0200

    changed pdf path

--- src/main/java/auebprogramming/GetPdf.java
+++ src/main/java/auebprogramming/GetPdf.java
@@ -11,7 +11,7 @@ public class GetPdf {
 
     public void fileDownloader(final String urlBudget, final int year) {
         String fileUrl = urlBudget;
-        String destinationFile = "budget" + year + ".pdf";
+        String destinationFile = "src/main/java/auebprogramming/resources/budget" + year + ".pdf";
 
         try {
             URL url = new URL(fileUrl);
