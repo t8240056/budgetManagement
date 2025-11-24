21feba4e293990a3f3ae27972bae0afb094e847c
Thanos Ilias <t8240051@aueb.gr>
2025-11-25 01:02:31 +0200

    updated app and changed budget path

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -3,12 +3,8 @@ package auebprogramming;
 /**entry point of the application */
 public class App {
 
-    //we dont want everytime we run the main to download the files from the start
-    static int getFilesOnce = 0;
     public static void main(final String[] args) {
-        if (getFilesOnce == 0 ) {
-            DownloadingOutputFiles.run();
-            getFilesOnce +=1;           
-        }
+        DownloadingOutputFiles.run();
+
     }
 }
