c56a0c59d72712054d38cd937d23c09074a15886
Theotrap06 <trapalis1924@gmail.com>
2025-11-24 16:51:44 +0200

    Edit of the file budgetManagement

--- src/main/java/auebprogramming/BudgetDisplay.java
+++ src/main/java/auebprogramming/BudgetDisplay.java
@@ -1,3 +1,16 @@
+/**
+ * Represents a component responsible for displaying
+ * budget-related information to the user.
+ *
+ * Any class that implements this interface must define
+ * how the budget data is displayed.
+ */
 public interface BudgetDisplay {
+
+    /**
+     * Displays the budget data.
+     * Implementing classes should specify the exact
+     * format and content of the output (e.g., tables, charts, text).
+     */
     void displayData();
 }
