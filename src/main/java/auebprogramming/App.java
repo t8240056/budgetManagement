e3a145ea402e24ea4598b8934a02a75e347e488c
Thanos Ilias <t8240051@aueb.gr>
2025-11-25 11:35:03 +0200

    run method printRevenues

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -6,9 +6,13 @@ public class App {
     //we dont want everytime we run the main to download the files from the start
     static int getFilesOnce = 0;
     public static void main(final String[] args) {
-        if (getFilesOnce == 0 ) {
+        /*if (getFilesOnce == 0 ) {
             DownloadingOutputFiles.run();
             getFilesOnce +=1;           
         }
+        */
+
+        RevenueExtractor re = new RevenueExtractor();
+        re.printRevenues();
     }
 }
