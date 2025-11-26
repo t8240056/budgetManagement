d8f5b9bd3128c32f3046c1a051072d4fbbd86b8e
Thanos Ilias <t8240051@aueb.gr>
2025-11-26 15:06:40 +0200

    got the only method in Links and added it in GetPdf

--- src/main/java/auebprogramming/GetPdf.java
+++ src/main/java/auebprogramming/GetPdf.java
@@ -8,10 +8,13 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class GetPdf {
-
-    public void fileDownloader(final String urlBudget, final int year) {
-        String fileUrl = urlBudget;
-        String destinationFile = "src/main/java/auebprogramming/resources/budget" + year + ".pdf";
+    /*
+        This is a method that download the budget for eatch year as a pdf 
+        It uses the static linksForBudget to get the correct url
+    */
+    public void fileDownloader(final int year) {
+        String fileUrl = GetPdf.linksForBudget(year);
+        String destinationFile = "src/main/resources/budget" + year + ".pdf";
 
         try {
             URL url = new URL(fileUrl);
@@ -22,5 +25,41 @@ public class GetPdf {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    static String urlBudget = "";
+
+    /*
+        this is a method that selects the correct link to work with
+    */
+    static String linksForBudget(final int year) throws IllegalArgumentException {
+        try { switch (year) {
+            case 2026:
+                urlBudget = "https://minfin.gov.gr/wp-content/uploads/2025/11/%CE%9A%CF%81%CE%B1%CF%84%CE%B9%CE%BA%CF%8C%CF%82-%CE%A0%CF%81%CE%BF%CF%8B%CF%80%CE%BF%CE%BB%CE%BF%CE%B3%CE%B9%CF%83%CE%BC%CF%8C%CF%82-2026.pdf";
+                return urlBudget;
+            case 2025: 
+                urlBudget = "https://minfin.gov.gr/wp-content/uploads/2024/11/%CE%9A%CF%81%CE%B1%CF%84%CE%B9%CE%BA%CF%8C%CF%82-%CE%A0%CF%81%CE%BF%CF%8B%CF%80%CE%BF%CE%BB%CE%BF%CE%B3%CE%B9%CF%83%CE%BC%CF%8C%CF%82-2025_%CE%9F%CE%95.pdf";
+                return urlBudget;
+            case 2024:
+                urlBudget = "https://minfin.gov.gr/wp-content/uploads/2023/11/%CE%9A%CE%A1%CE%91%CE%A4%CE%99%CE%9A%CE%9F%CE%A3-%CE%A0%CE%A1%CE%9F%CE%A5%CE%A0%CE%9F%CE%9B%CE%9F%CE%93%CE%99%CE%A3%CE%9C%CE%9F%CE%A3-2024.pdf";
+                return urlBudget;
+            case 2023:
+                urlBudget = "https://minfin.gov.gr/wp-content/uploads/2023/11/21-11-2022-%CE%9A%CE%A1%CE%91%CE%A4%CE%99%CE%9A%CE%9F%CE%A3-%CE%A0%CE%A1%CE%9F%CE%AB%CE%A0%CE%9F%CE%9B%CE%9F%CE%93%CE%99%CE%A3%CE%9C%CE%9F%CE%A3-2023.pdf";
+                return urlBudget;
+            case 2022:
+                urlBudget = "https://minfin.gov.gr/wp-content/uploads/2023/11/%CE%9A%CE%A1%CE%91%CE%A4%CE%99%CE%9A%CE%9F%CE%A3-%CE%A0%CE%A1%CE%9F%CE%A5%CE%A0%CE%9F%CE%9B%CE%9F%CE%93%CE%99%CE%A3%CE%9C%CE%9F%CE%A3_2022.pdf";
+                return urlBudget;
+            case 2021:
+                urlBudget = "https://minfin.gov.gr/wp-content/uploads/2023/11/%CE%9A%CE%A1%CE%91%CE%A4%CE%99%CE%9A%CE%9F%CE%A3-%CE%A0%CE%A1%CE%9F%CE%A5%CE%A0%CE%9F%CE%9B%CE%9F%CE%93%CE%99%CE%A3%CE%9C%CE%9F%CE%A3-2021.pdf";
+                return urlBudget;
+            case 2020:
+                urlBudget = "https://minfin.gov.gr/wp-content/uploads/2019/11/21-11-2019-%CE%9A%CE%A1%CE%91%CE%A4%CE%99%CE%9A%CE%9F%CE%A3-%CE%A0%CE%A1%CE%9F%CE%A5%CE%A0%CE%9F%CE%9B%CE%9F%CE%93%CE%99%CE%A3%CE%9C%CE%9F%CE%A3-2020.pdf";
+                return urlBudget;
+            }
+        } catch (IllegalArgumentException e) {
+            return "wrong year, choose from 2020 - 2025";
+        }
+        return "";
+
     }
 }
