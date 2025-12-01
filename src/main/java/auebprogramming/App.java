86a57ad784cde030a3178861f164746deda85c8b
Nikolas Kalantzis <t8240056@aueb.gr>
2025-12-01 19:45:42 +0200

    Added MainFrame to main method

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -1,22 +1,14 @@
 package auebprogramming;
 
-import java.util.Scanner;
 
 /**entry point of the application */
 public class App {
 
     public static void main(final String[] args) {
-        // DownloadingOutputFiles.run();  run only once 
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
         
-        Scanner scanner = new Scanner(System.in);
-                
-        System.out.println(" Which year do you want to see ?");
-        int year = scanner.nextInt();
        
-        
-        scanner.close();
-
-        Article_1new.printArticle1New(year);
         
     }
 }
