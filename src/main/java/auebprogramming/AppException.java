68fd8de52a6470c270c6aa484c64b665375584f3
Nikolas Kalantzis <t8240056@aueb.gr>
2025-12-03 14:58:10 +0200

    Wrote the AppException class

--- src/main/java/auebprogramming/AppException.java
+++ src/main/java/auebprogramming/AppException.java
@@ -1,5 +1,34 @@
 package    auebprogramming;
 
+import javax.swing.JOptionPane;
+
+/**
+ * Custom application exception class.
+ * Provides a static helper method for displaying Swing error dialogs.
+ */
 public class AppException extends Exception {
+    /**
+     * Creates a new AppException with the specified detail message.
+     *
+     * @param message the detail message to be associated with this exception
+     */
+    public AppException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Displays an error dialog using JOptionPane.
+     * Intended for use when catching application-level exceptions.
+     *
+     * @param message the error message to display
+     */
+    public static void showError(final String message) {
+        JOptionPane.showMessageDialog(
+            null,
+            message,
+            "Σφάλμα",
+            JOptionPane.ERROR_MESSAGE
+        );
+    }
 
 }
