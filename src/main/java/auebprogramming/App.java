0699f16a799d30f754e4f238a710ab38bae4b9ae
Nikolas Kalantzis <t8240056@aueb.gr>
2025-12-04 22:28:08 +0200

    Fixed Checkstyle violations

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -1,8 +1,8 @@
 package auebprogramming;
 
-/** entry point of the application */
-public class App {
-
+/** entry point of the application. */
+public final class App {
+/** entry point of the application. */
     public static void main(final String[] args) {
         MainFrame mainFrame = new MainFrame();
         mainFrame.setVisible(true);
