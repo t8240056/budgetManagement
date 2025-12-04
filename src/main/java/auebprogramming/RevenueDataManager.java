da61d8249bbf50e994bf9d2328562b1551e7b898
Thanasis Bliachos <t8240086@aueb.gr>
2025-12-04 12:35:40 +0200

    Creation of 4 string arrays

--- src/main/java/auebprogramming/RevenueDataManager.java
+++ src/main/java/auebprogramming/RevenueDataManager.java
@@ -6,6 +6,10 @@ import java.util.List;
 /**
  * Manages budget data from CSV files.
  */
-public final class RevenueDataManager{
-    
+public final class RevenueDataManager {
+
+    private final String[][] codes2; // 2-digit codes
+    private final String[][] codes3; // 3-digit codes
+    private final String[][] codes5; // 5-digit codes
+    private final String[][] codes7; // 7-digit codes
 }
