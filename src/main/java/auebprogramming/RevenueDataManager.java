8ce27e64e9bcae353eaddf7bf580d983f6c4c55a
Thanasis Bliachos <t8240086@aueb.gr>
2025-12-04 12:40:57 +0200

    Creation of constructor that loads csv files to each array

--- src/main/java/auebprogramming/RevenueDataManager.java
+++ src/main/java/auebprogramming/RevenueDataManager.java
@@ -12,4 +12,14 @@ public final class RevenueDataManager {
     private final String[][] codes3; // 3-digit codes
     private final String[][] codes5; // 5-digit codes
     private final String[][] codes7; // 7-digit codes
+
+    /**
+     * Creates a manager and loads all CSV files.
+     */
+    public RevenueDataManager() {
+        this.codes2 = CsvToArray.loadCsvToArray("revenue_categories2_2025.csv");
+        this.codes3 = CsvToArray.loadCsvToArray("revenue_categories3_2025.csv");
+        this.codes5 = CsvToArray.loadCsvToArray("revenue_categories5_2025.csv");
+        this.codes7 = CsvToArray.loadCsvToArray("revenue_categories7_2025.csv");
+    }
 }
