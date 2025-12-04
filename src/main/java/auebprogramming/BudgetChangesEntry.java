142a8ec3dae357b143c67d8ccd645813467e21f2
Kostaspapakon <t8240275@aueb.gr>
2025-12-05 01:15:26 +0200

    made a constructor for BudgetChangesEntry

--- src/main/java/auebprogramming/BudgetChangesEntry.java
+++ src/main/java/auebprogramming/BudgetChangesEntry.java
@@ -5,4 +5,16 @@ import java.util.Objects;
 
 public class BudgetChangesEntry {
     
+
+    private final String code;          
+    private final String description;   
+    private BigDecimal amount; 
+
+    // Constructs a new BudgetEntry with the given code, description and amount
+    public BudgetChangesEntry(String code, String description, BigDecimal amount) {
+        this.code = Objects.requireNonNull(code, "Code cannot be null");
+        this.description = Objects.requireNonNull(description, "Description cannot be null");
+        this.amount = Objects.requireNonNull(amount, "Amount cannot be null");
+    }
+
 }
