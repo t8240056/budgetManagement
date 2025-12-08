9a53bef34c92949f12d8133d20d97f9647c03cf6
Nikolas Kalantzis <t8240056@aueb.gr>
2025-12-08 17:51:56 +0200

    Changed table's size in Revenue2Panel

--- src/main/java/auebprogramming/Revenue2Panel.java
+++ src/main/java/auebprogramming/Revenue2Panel.java
@@ -3,6 +3,7 @@ package auebprogramming;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,6 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.JTableHeader;
 
 /**
  * Panel responsible for displaying second-level revenue analysis based
@@ -67,8 +69,17 @@ public final class Revenue2Panel extends JPanel {
         String[] columnNames = { "Κωδικός", "Κατηγορία","Ποσό" };
 
         revenueTable = new JTable(emptyData, columnNames);
+        // Μεγαλύτερη γραμματοσειρά για τα κελιά
+        revenueTable.setFont(new Font("Arial", Font.PLAIN, 16));
+        // Μεγαλύτερο ύψος γραμμής
+        revenueTable.setRowHeight(24);
+
+        // Μεγαλύτερη γραμματοσειρά για τις επικεφαλίδες
+        JTableHeader header = revenueTable.getTableHeader();
+        header.setFont(new Font("Arial", Font.BOLD, 18));
+
         JScrollPane scrollPane = new JScrollPane(revenueTable);
-        scrollPane.setPreferredSize(new Dimension(500, 500));
+        scrollPane.setPreferredSize(new Dimension(500, 300));
         centerPanel.add(scrollPane);
         add(centerPanel, BorderLayout.CENTER);
     }
@@ -123,7 +134,7 @@ public final class Revenue2Panel extends JPanel {
             public void actionPerformed(final ActionEvent event) {
                 if (codeField.isVisible() && !codeField.getText().trim().isEmpty()) {
                 String code = getCode3();
-                //frame.showRevenue3(code);
+                frame.showRevenue3(code);
             } else {
                 AppException.showError(
                     "Πληκτρολογήστε κωδικό ή πατήστε Επιστροφή.");
