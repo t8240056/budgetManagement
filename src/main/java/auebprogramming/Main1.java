52a8b8dc38d14772f5bcefe23dbde1c77081b189
= <t8240076@aueb.gr>
2025-12-13 15:24:28 +0200

    Modify Main1 file

--- src/main/java/auebprogramming/Main1.java
+++ src/main/java/auebprogramming/Main1.java
@@ -1,25 +1,13 @@
 package auebprogramming;
 
-import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Main1 {
     public static void main(String[] args) {
-    // 1. Δημιουργία της Αποθήκης (Κενή)
-    BudgetRepository repo = new BudgetRepository();
+        int chooseBudgetType; //0 for revenue, 1 for expense
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please choose budget type (0 for revenue, 1 for expense): ");
+        chooseBudgetType = scanner.nextInt();
 
-    // 2. Φόρτωση από το αρχείο (Η Γέφυρα δουλεύει εδώ)
-    BudgetIOHandler.loadDataFromFile("my_budget.csv", repo);
-
-    // 3. Κάνουμε δουλειά με το "Έξυπνο Σύστημα" (Ομάδα Α)
-    // Βρίσκουμε μια εγγραφή
-    BudgetChangesEntry food = repo.findByCode("FOOD-01").orElseThrow();
-    
-    // Κάνουμε μια αλλαγή
-    BudgetChange increase = new AbsoluteAmountChange(food.getCode(), new BigDecimal("50.0"), "Bonus", "User1");
-    increase.apply(food); // Το ποσό άλλαξε στη μνήμη
-
-    // 4. Αποθήκευση πίσω στο αρχείο (Η Γέφυρα δουλεύει πάλι)
-    // Θα πάρει τα νέα δεδομένα από το Repo και θα τα γράψει στο CSV
-    BudgetIOHandler.saveDataToFile("my_budget.csv", repo);
     }
 }
