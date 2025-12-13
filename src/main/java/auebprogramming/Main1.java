35bbde9ae736d8175f313f99a0df8a57e5707199
Thanos Ilias <t8240051@aueb.gr>
2025-12-13 15:38:22 +0200

    print revenue categories

--- src/main/java/auebprogramming/Main1.java
+++ src/main/java/auebprogramming/Main1.java
@@ -1,5 +1,8 @@
 package auebprogramming;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main1 {
@@ -11,11 +14,26 @@ public class Main1 {
 
         if (chooseBudgetType == 0) { //Revenue
             //Display revenue categories
-            
+            try {
+                FileReader reader = new FileReader("src/main/resources/revenue_categories2_2025.csv");
+                try {
+                    int data = reader.read();
+                    while (data != -1) {
+                        System.out.print((char)data);
+                        data = reader.read();
+                    } 
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         } else if (chooseBudgetType == 1) { //Expense
             //Display expense categories
         } else {
             System.out.println("Invalid choice. Please enter 0 or 1.");
         }
+        System.out.println();
     }
 }
