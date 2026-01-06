1f6254643fb629f569ead2028db717af8b358899
t8240056 <t8240056@aueb.gr>
2026-01-06 20:46:06 +0200

    Fixed Chekstyle violations

--- src/main/java/auebprogramming/App.java
+++ src/main/java/auebprogramming/App.java
@@ -2,18 +2,21 @@ package auebprogramming;
 
 /** entry point of the application. */
 public final class App {
-/** private constructor.
- */
-private App() {
+    /**
+     * private constructor.
+     */
+    private App() {
 
-}
-/** entry point of the application.
- * @param args
- */
+    }
+
+    /**
+     * entry point of the application.
+     *
+     * @param args
+     */
     public static void main(final String[] args) {
         MainFrame mainFrame = new MainFrame();
         mainFrame.setVisible(true);
-        
 
     }
 }
